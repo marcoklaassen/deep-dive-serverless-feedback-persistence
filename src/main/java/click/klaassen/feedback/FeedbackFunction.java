02ef65313ec32b75e9b8d5b69e02e4f69ff5d3c6
Marco Klaassen <mklaasse@redhat.com>
2022-11-17 13:16:50 +0100

    fix implementation and remove unused kafka dep

--- src/main/java/click/klaassen/feedback/FeedbackFunction.java
+++ src/main/java/click/klaassen/feedback/FeedbackFunction.java
@@ -3,14 +3,10 @@ package click.klaassen.feedback;
 import io.quarkus.funqy.Funq;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import static click.klaassen.feedback.Feedback.builder;
-
-@ApplicationScoped
 @Slf4j
 public class FeedbackFunction {
 
@@ -19,8 +15,9 @@ public class FeedbackFunction {
 
     @Transactional
     @Funq
-    public void feedback(Integer rating) {
+    public Integer feedback(Integer rating) {
         log.info("Feedback persistence got rating: {}", rating);
-        em.persist(builder().rating(rating).build());
+        em.persist(Feedback.builder().rating(rating).build());
+        return rating;
     }
 }
